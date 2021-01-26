b1838c60715e69845e0a280dd45c4bc07e318be8
abx <whitexdevil09@gmail.com>
2021-01-26 20:16:36 +0530

    discord bot 1

---
+++ discordbot1.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', msg => {
+  if (msg.content === 'bhwu') {
+    msg.reply('i lobe u bhut saaaaraa');
+  }
+  if (msg.content === 'sex?') {
+    msg.reply('abhi ni raat me, jungle me mangalll');
+  }
+  if (msg.content === 'who is chuzi?') {
+    msg.reply('obsv man! bhwnaaaaaaaa! the hot chick! on planet');
+  }
+  if (msg.content === 'who is dugu?') {
+    msg.reply('chuuuzzi ka deewana! sabse jabra fan! who loveeeeeeee to drink milk. cow milk* 😎😎');
+  }
+  if (msg.content === 'i made this baby') {
+    msg.reply('dont call me baby u sexist!!!');
+  }
+  if (msg.content === 'lets ficken') {
+    msg.reply('aaja gand marle');
+  }
+  if (msg.content === 'nahi') {
+    msg.reply('oooohhhhhh ladki ky mili u frgt me!!!');
+  }
+
+});
+ 
+client.login('Nzk3NDI2MjY2ODAyMzU2MjI0.X_mTFQ.nATSCEAhspeSALnpeWqodBUDFr4');
